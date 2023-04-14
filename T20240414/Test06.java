ca07dd3b1d38d9c470e84e7e81f6f0d0fa5ab067
kanggicheon <snoopy1516@naver.com>
2023-04-14 16:55:29 +0900

    *과 자릿수 출력

---
+++ T20240414/Test06.java
@@ -0,0 +1,24 @@
+package T20240414;
+
+import java.util.Scanner;
+
+public class Test06 {
+
+	public static void main(String[] args) {
+		
+		Scanner stdin = new Scanner(System.in);
+		System.out.print("몇 개의 *를 표시할까요?:");
+		int n = stdin.nextInt();
+		
+		if (n > 0) {
+			int i = 0;
+			while(i < n) {
+				System.out.print('*');
+				i++;
+			}
+			System.out.println();
+		}
+
+	}
+
+}
